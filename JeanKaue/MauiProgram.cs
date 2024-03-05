039c47bff6ec3f533fc9538a1f18972d1fbb05e0
JeanKaue <jeangustavofut@gmail.com>
2024-03-05 11:52:26 -0300

    trabalho app

--- JeanKaue/MauiProgram.cs
+++ JeanKaue/MauiProgram.cs
@@ -11,8 +11,8 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Mercado.ttf", "Mercado");
+				fonts.AddFont("Payback.ttf", "Payback");
 			});
 
 #if DEBUG
